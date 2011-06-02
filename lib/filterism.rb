9d814ac2c7c28d0e56b7c9b2bbfe3f7d620710eb
Phil Spitler <pspitler@gmail.com>
2011-06-02 17:15:01 -0400

    wrote initial code and tests

--- lib/filterism.rb
+++ lib/filterism.rb
@@ -1,3 +1,57 @@
+require 'active_record'
+
 module Filterism
-  # Your code goes here...
+  COMPARATORS = {
+    'equal_to' => '=',
+    'not_equal_to' => '!=',
+
+    'greater_than' => '>',
+    'gt' => '>',
+    'after' => '>',
+    'greater_than_or_equal_to' => '>=',
+    'gtet' => '>=',
+
+    'less_than' => '<',
+    'lt' => '<',
+    'before' => '<',
+    'less_than_or_equal_to' => '<=',
+    'ltet' => '<=',
+
+    'like' => 'LIKE'
+  }
+  
+  def filter(params)
+
+    #remove non filter params
+    params.delete_if { |key, value| key.to_s.index('_is_').nil? }
+
+    where_clause = ""
+    values = []
+
+    params.each do |key, value|
+      keysplit = key.to_s.split('_is_')
+      column = keysplit[0]
+      comparator = COMPARATORS[keysplit[1]]
+
+      where_clause << " AND" unless where_clause.empty?
+      where_clause << " #{column} #{comparator} ?"
+      if comparator == 'LIKE'
+        values << "%#{value}%"
+      else
+        if value.index(/true|false/)
+          values << true if value == "true"
+          values << false if value == "false"
+        else
+          values << value
+        end
+      end
+
+    end
+
+    where(where_clause, *values)
+
+  end
+
 end
+
+ActiveRecord::Base.extend Filterism
--- spec/filterism_spec.rb
+++ spec/filterism_spec.rb
@@ -6,8 +6,62 @@ describe Filterism do
   extend ActiverecordTestConnector::FixtureSetup
   fixtures :users
 
-  it "should be a test" do
-    User.find(1).id.should == 1
+  it "defines the COMPARATORS constant" do
+    Filterism::COMPARATORS.should be
+    Filterism::COMPARATORS.should be_a Hash
+  end
+
+  it "fitlers on equal_to" do
+    User.filter({'name_is_equal_to' => 'David'}).first.id.should == 1
+    User.filter({'name_is_equal_to' => 'Dave'}).first.id.should == 2
+  end
+
+  it "filters on not_equal_to" do
+    User.filter({'name_is_not_equal_to' => 'David'}).all.count.should == 4
+  end
+
+  it "filters on greater_than" do
+    User.filter({'salary_is_greater_than' => '80000'}).all.count.should == 1
+  end
+
+  it "filters on gt" do
+    User.filter({'salary_is_gt' => '70000'}).all.count.should == 2
+  end
+
+  it "filters on after" do
+    User.filter({'salary_is_after' => '9000'}).all.count.should == 4
+  end
+
+  it "filters on greater_than_or_equal_to" do
+    User.filter({'salary_is_greater_than_or_equal_to' => '80000'}).all.count.should == 2
+  end
+
+  it "filters on gtet" do
+    User.filter({'salary_is_gtet' => '80000'}).all.count.should == 2
+  end
+
+  it "filters on less_than" do
+    User.filter({'salary_is_less_than' => '80000'}).all.count.should == 3
+  end
+
+  it "filters on lt" do
+    User.filter({'salary_is_lt' => '70000'}).all.count.should == 3
+  end
+
+  it "filters on before" do
+    User.filter({'salary_is_before' => '10000'}).all.count.should == 1
+  end
+
+  it "filters on less_than_or_equal_to" do
+    User.filter({'salary_is_less_than_or_equal_to' => '80000'}).all.count.should == 4
+  end
+
+  it "filters on ltet" do
+    User.filter({'salary_is_ltet' => '80000'}).all.count.should == 4
+  end
+
+  it "filters on like" do
+    User.filter({'name_is_like' => 'Dav'}).all.count.should == 2
   end
 
 end
